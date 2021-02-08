0e0a39e3c9bbce2b7960d418b38a378fb58a0865
johltn <j.luttun@laposte.net>
2021-02-08 09:33:37 +0100

    Get IFC schema version

--- parse_file.py
+++ parse_file.py
@@ -8,7 +8,6 @@ file: "ISO-10303-21;" header data "END-ISO-10303-21;"
 
 header: "HEADER" ";" filerecord* "ENDSEC" ";"
 
-
 data: "DATA" ";" record* "ENDSEC" ";"
 filerecord: filedecl attributes ";"
 record: id "=" ifcclass attributes ";"
@@ -86,18 +85,23 @@ WS: /[ \t\f\r\n]/+
 
 f = open("files/Duplex_A_20110505.ifc", "r")
 
-text = f.read() 
+#f = open("files/acad2010_walls.ifc", "r")
 
+text = f.read() 
 
 start_time = time.time()
 tree = ifc_parser.parse(text)
 
-print(len(tree.children))
-
-for c in tree.children:
-        print(c.data)
-
 print("--- %s seconds ---" % (time.time() - start_time))
 
+header = tree.children[0]
 
+for filerecord in header.children:
+        if filerecord.children[0].children[0] == 'SCHEMA':
+                schema_tree = filerecord.children[1]
+                schema_string = schema_tree.children[0].children[0].children[0].children[0]
+                char_list = [c[0] for c in schema_string.children ]
+                schema = "".join(char_list)     
+
+data = tree.children[1]
 
