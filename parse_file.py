2376bae30888fe644917cfa68372a8a117ac1247
johltn <j.luttun@laposte.net>
2021-02-24 09:06:02 +0100

    Handle relations and plot IFC graph with NetworkX

--- parse_file.py
+++ parse_file.py
@@ -83,8 +83,8 @@ WS: /[ \t\f\r\n]/+
 
 """, parser='lalr', start='file')
 
-f = open("files/Duplex_A_20110505.ifc", "r")
 
+f = open("files/acad2010_walls.ifc", "r")
 text = f.read() 
 
 start_time = time.time()
@@ -104,6 +104,11 @@ for filerecord in header.children:
 data = tree.children[1]
 
 test_record = data.children[0]
+
+
+rels = []
+
+drels = {}
       
 class IfcType:
         def __init__(self, ifctype, value):
@@ -137,19 +142,31 @@ class T(Transformer):
         NONE = str
         STAR = str
 
-
-def process_attributes(attributes_tree, tup=False):
+def process_attributes(file_id, attributes_tree, tup=False):
         attributes = []
+        relations = []
+        
         for a in attributes_tree.children:
                 try:     
                         if a.children[0].data == 'tup':
-                                attributes.append(process_attributes(a.children[0], tup=True))
+                                attributes.append(process_attributes(file_id, a.children[0], tup=True))
                         elif a.children[0].data == 'string' :
                                 to_append = T(visit_tokens=True).transform(a)
                                 attributes.append(to_append.children[0])
                         elif a.children[0].data == 'id' :
                                 to_append = T(visit_tokens=True).transform(a)
                                 attributes.append(to_append.children[0])
+                                relations.append(to_append.children[0])
+                                
+                                if tup:
+                                        # print(file_id, to_append.children[0])
+                                        rels.append((file_id, to_append.children[0],))
+
+                                        if file_id in drels.keys():
+                                                drels[file_id].append(to_append.children[0])
+                                        else:
+                                                drels[file_id] = [to_append.children[0]]
+
                         elif a.children[0].data == 'enumeration' :
                                 # attributes.append(a.children[0].children[0])
                                 to_append = T(visit_tokens=True).transform(a).children[0]
@@ -166,7 +183,8 @@ def process_attributes(attributes_tree, tup=False):
         if tup:
                 return tuple(attributes)
         else:
-                return attributes
+                return attributes,relations
+
 
 
 def create_entity(record, is_attr=False):
@@ -183,7 +201,7 @@ def create_entity(record, is_attr=False):
                 
                 ifc_type = "IFC" + record.children[1].children[0]
                 attributes_tree = record.children[2]
-                attributes = process_attributes(attributes_tree)
+                attributes = process_attributes(file_id, attributes_tree)
 
                 return {'id':file_id, 'ifc_type':ifc_type, 'attributes':attributes }
 
@@ -193,5 +211,105 @@ ents = {}
 for r in data.children:
         entity = create_entity(r)
         ents[entity['id']] = entity
+
+
+for e in ents.values():
+        if e['id'] in drels.keys():
+                e['attributes'][1].extend(drels[e['id']])
+
+class IfcEntity:
+        def __init__(self, data):
+                self.ifctype = data['ifc_type']
+                self.id = data['id']
+                
+        def __str__(self):
+                return "#" + str(self.id) + "_" + self.ifctype
+        __repr__ = __str__
+
+import matplotlib.pyplot as plt
+import networkx as nx
+from itertools import count
+import matplotlib.patches as mpatches
+from matplotlib import pylab
+
+def get_cmap(n, name='hsv'):
+    '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct 
+    RGB color; the keyword argument name must be a standard mpl colormap name.'''
+    return plt.cm.get_cmap(name, n)
+
+
+G = nx.Graph()
+
+colors = []
+color_map = []
+
+ifctypes = set()
+
+types_color = {}
+
+for e in ents.values():
+        for r in e['attributes'][1]:
+                G.add_edge(e['id'], r)
+                color_map.append('red')
+                ifctypes.add(e['ifc_type'])
+                ifctypes.add(ents[r]['ifc_type'])
+                
+
+cmap = get_cmap(len(ifctypes),'rainbow')
+
+colors = [cmap(i) for i in range(len(ifctypes))]
+
+type_color_mapping = {}
+
+cols = []
+
+i = 0
+for eid in G.nodes():
+        t = ents[eid]['ifc_type']
+        # print(t)
+        if t in type_color_mapping.keys():
+                cols.append(type_color_mapping[t])
+        else:
+                type_color_mapping[t] = colors[i]
+                i = i + 1
+                cols.append(type_color_mapping[t])
+
+
+# k controls the distance between the nodes and varies between 0 and 1
+# iterations is the number of times simulated annealing is run
+# default k=0.1 and iterations=50
+pos = nx.spring_layout(G, k=0.8, iterations=60)  # positions for all nodes
+
+nodes = G.nodes()
+print(get_cmap(len(nodes)))
+cmap = get_cmap(len(nodes))
+
+c_map = [cmap(i) for i in range(len(nodes))]
+
+nc = nx.draw_networkx_nodes(G, pos,nodelist=nodes, node_color=cols,node_size=200)
+
+# edges
+elarge = [(u, v) for (u, v, d) in G.edges(data=True) ]
+nx.draw_networkx_edges(G, pos,edgelist=elarge, width=1)
+
+nx.draw_networkx_labels(G, pos, font_size=8, font_family="sans-serif")
+
+
+red_patch = mpatches.Patch(color='red', label='The red data')
+blue_patch = mpatches.Patch(color='blue', label='The blue data')
+
+patches = []
+for k,v in type_color_mapping.items():
+        patches.append(mpatches.Patch(color=v, label=k))
+
+
+plt.legend(handles=patches,fontsize=8)
+plt.axis("off")
+plt.show()
+
+
+
+
+
 
 
